13c601dbdd696b02fa9f11d458908e1b79d17a71
Chepngeno-langat <langatkarenn@gmail.com>
2024-05-30 15:30:10 +0300

    containerization

--- House_prices_prediction/app/app.py
+++ House_prices_prediction/app/app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
+import logging
 import pandas as pd
 import math
 import os
 
 app = Flask(__name__)
-
-# model = joblib.load('/app/notebooks/gb_model.pkl')
-# model_columns = joblib.load('/app/notebooks/model_columns.pkl')
-# one_hot_encoder = joblib.load('/app/notebooks/one_hot_encoder.pkl')
+logging.basicConfig(level=logging.INFO)
 
 # Adjust the paths to be relative to the app directory
 model_path = os.path.join(os.path.dirname(__file__), '../notebooks/gb_model.pkl')
+try:
+    model = joblib.load(model_path)
+    logging.info("Model loaded successfully")
+except Exception as e:
+    logging.error(f"Error loading model: {e}")
+    raise
+
 model_columns_path = os.path.join(os.path.dirname(__file__), '../notebooks/model_columns.pkl')
 one_hot_encoder_path = os.path.join(os.path.dirname(__file__), '../notebooks/one_hot_encoder.pkl')
 
